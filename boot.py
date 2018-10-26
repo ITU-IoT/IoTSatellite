1fb81efdb47706f8265b4de92660b2d5fb158aca
Philip Korsholm <phko@itu.dk>
2018-10-26 15:40:51 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -3,8 +3,8 @@ from network import WLAN
 import pycom
 from wifi import wifiSetup
 
-SSID = "verynice"
-PWD = "borat1337"
+SSID = "NETGEAR"
+PWD = "hackerMAN158"
 
 pycom.heartbeat(False)
 pycom.rgbled(0xff7f00)
